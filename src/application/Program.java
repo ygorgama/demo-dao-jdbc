06db27e22efea8f6e5fa3d580e1c8e03bdab9113
Ygor Pinto Gama <ygor.bio.2002@gmail.com>
2023-11-04 10:17:46 -0300

    updated program class and created seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.time.LocalDateTime;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1 , "Department");
-		
+		Seller obj2 = new Seller(21, "Bob", "bob@gmail.com", LocalDateTime.now(), 3000.00, obj);
 		System.out.println(obj.toString());
+		System.out.println(obj2.toString());
 	}
 }
