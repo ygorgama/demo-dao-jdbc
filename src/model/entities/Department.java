4be486b734e3c1cc7131827dc58ad26391a27fc1
Ygor Pinto Gama <ygor.bio.2002@gmail.com>
2023-11-04 10:03:32 -0300

    Class Department created

---
+++ src/model/entities/Department.java
@@ -0,0 +1,69 @@
+package model.entities;
+
+import java.util.Objects;
+
+public class Department {
+	private Integer id;
+	private String name;
+	
+	public Department() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Department(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Department [id = " + id + ", name = " + name + "]";
+	}
+	
+	
+	
+	
+}
