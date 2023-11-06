bc8709771ff4d3f19a9f1327f4ed43a7c583be7f
Ygor Pinto Gama <ygorpintogama@pop-os.localdomain>
2023-11-06 05:56:03 -0300

    Update method implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,8 +16,11 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findAll();
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+//		Seller newSeller = new Seller(9, "Romildo", "romildo@gmail.com", new Date(), 3000.00, department);
+//		sellerDao.update(newSeller);
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		
 	}
 }
