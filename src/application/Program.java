172f0199d75c70e721045f0f149b9ecf41b7aba1
Ygor Pinto Gama <ygorpintogama@pop-os.localdomain>
2023-11-06 05:04:05 -0300

    Find Seller by department implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,23 @@
 package application;
 
-import java.time.LocalDateTime;
 
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		Department obj = new Department(1 , "Department");
-		Seller obj2 = new Seller(21, "Bob", "bob@gmail.com", LocalDateTime.now(), 3000.00, obj);
-		System.out.println(obj.toString());
-		System.out.println(obj2.toString());
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		System.out.println(seller);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,7 +1,7 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Objects;
 
 public class Seller implements Serializable{
@@ -12,7 +12,7 @@ public class Seller implements Serializable{
 	private Integer id;
 	private String name;
 	private String email;
-	private LocalDateTime birthDate;
+	private Date birthDate;
 	private Double baseSalary;
 	private Department department;
 	
@@ -20,7 +20,7 @@ public class Seller implements Serializable{
 		// TODO Auto-generated constructor stub
 	}
 
-	public Seller(Integer id, String name, String email, LocalDateTime birthDate, Double baseSalary,
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary,
 			Department department) {
 		this.id = id;
 		this.name = name;
@@ -54,11 +54,11 @@ public class Seller implements Serializable{
 		this.email = email;
 	}
 
-	public LocalDateTime getBirthDate() {
+	public Date getBirthDate() {
 		return birthDate;
 	}
 
-	public void setBirthDate(LocalDateTime birthDate) {
+	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
 
